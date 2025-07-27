a1b725a5fc3a9f1b080d0e3ea61d2636c3ea8d5b
arnaly <gilgilarnau@gmail.com>
2025-07-27 14:37:15 +0200

    configurar servidor para servir archivos estáticos

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import { reqWeather } from "./weather.controller.js";
 
@@ -6,9 +8,19 @@ dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/api/weather", reqWeather);
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
